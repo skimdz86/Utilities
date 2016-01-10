7a327394898a3adbb41a38259d7a93f688e1fd1b
mdz <skimdz86@gmail.com>
2016-01-10 14:56:02 +0100

    finished change date script

--- Shell scripts/file_change_date.sh
+++ Shell scripts/file_change_date.sh
@@ -1,4 +1,12 @@
-##
+## change the image creation time (only change hours for now, so that we can manage the different timezone)
+
+if [ $# -lt 1 ]
+  then
+    echo "No arguments supplied"
+    echo "Usage: ./file_change_date.sh <HOUR_NUMBER>"
+    echo "e.g. ./file_change_date.sh -5 or ./file_change_date.sh 7"
+    exit 1
+fi
 
 offset=$1
 direction=POS
@@ -8,19 +16,22 @@ direction=POS
 if [[ $offset == -* ]]
 then
   direction=NEG
-  offset=${offset:1}
+  #offset=${offset:1}
 fi
 
 echo "OFFSET: $direction $offset"
 
 for i in $(find . -iname "*.jpg")
 do
-  mod_date=`stat $i | grep Modify | grep -Eo "2015.*?[^00000000]"`
-  mod_date=${mod_date%?}
+  echo "$i"
+  mod_date=`stat $i | grep Modif | sed 's/\.000000000.*//g' | sed 's/\(.*\)\([0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\} [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\)/\2/g'`
   echo "mod_date: $mod_date"
   #eg. date: 2015-11-21 19:09:18
   epoch_secs=`date +%s -d "$mod_date"`
-  echo "EPOCH: $epoch_secs"
-  NEXT_DATE=`date "+%Y-%m-%d %k:%M:%S" --date="@$((epoch_secs + 3600))"`
+  #echo "EPOCH: $epoch_secs"
+  increment=$((offset * 3600))
+  #echo "increment $increment"
+  NEXT_DATE=`date "+%Y-%m-%d %k:%M:%S" --date="@$((epoch_secs + increment))"`
   echo "NEXT: $NEXT_DATE"
+  touch -d "$NEXT_DATE" $i
 done;
